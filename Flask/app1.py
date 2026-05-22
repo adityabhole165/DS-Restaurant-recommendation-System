cacc7bd62c80cf414d710199bf11d746a54d1299
Aditya Bhole <130380292+adityabhole165@users.noreply.github.com>
2026-05-22 14:45:32 +0530

    Update app1.py

--- Flask/app1.py
+++ Flask/app1.py
@@ -76,8 +76,7 @@ def result():
         return redirect(url_for('home'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000))
-
+     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 
 
 
